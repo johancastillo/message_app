a5182243324310a3cbc14df389cac99abb25ab7a
Johan Castillo <jcjohan2707@gmail.com>
2024-07-07 01:52:12 -0400

    Crear estructura para trabajar con provider

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:messages_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier{
+  List<Message> messages = [
+    Message(text: "Hola amor", fromWho: FromWho.me),
+    Message(text: "Regresaste del trabajo?", fromWho: FromWho.me),
+  ];
+
+
+  Future<void> sendMessage(String text) async{
+    // TODO: Implementar metodo
+  }
+
+}
